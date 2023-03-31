50de77157268a1847c82c3da54f547e769770e20
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2023-03-31 17:37:26 +0900

    implement the add and remove btn inside the Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -24,28 +24,50 @@ const cartReducer = (state, action) => {
 
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
-    
+
     if (existingCartItem) {
-        const updatedItem = {
-            ...existingCartItem,
-            amount: existingCartItem.amount + action.item.amount,
-        };
-     
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
 
-        //instead of reassining only the fields needed, I'm overwriting the whole object
-        //to use the non-mutational approach
-        updatedItems = [...state.items];
-        updatedItems[existingCartItemIndex] = updatedItem;
+      //instead of reassining only the fields needed, I'm overwriting the whole object
+      //to use the non-mutational approach
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
-        updatedItems = state.items.concat(action.item);
+      updatedItems = state.items.concat(action.item);
     }
-
 
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+        updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+        const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+        updatedItems = [ ...state.items ];
+        updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
