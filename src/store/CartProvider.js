0ebb9683f2d2a2d263b57393e20f31043cce98e8
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2023-03-31 13:58:29 +0900

    create the useReducer logic and one action for add items in the cart
    the useReducer accepts two arguments, the first is a
    reducer function to be called when the hook is called
    and the second is the initial state
    
    the function passed to the hook accepts two arguments.
    the first is a snapshot of the state and the second is
    the action, which is recommended to be an object.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,27 +1,67 @@
-import CartContext from './cart-context';
+import { useReducer } from "react";
+import CartContext from "./cart-context";
 
-//the objective of this component is to alter the CartContext and 
-//provide its content to the rest of the application
-const CartProvider = props => {
+//create a default state to be assign when the component is loaded for the first time
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-    const addItemToCartHandler = item => {};
-
-    const removeItemFromCartHandler = id => {};
-
-
-
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler,
+//create a function to act as the reducer in the useReducer hook,
+//because the logic is a little complex
+//the reducer function receives two arguments from react, the current state and the action
+const cartReducer = (state, action) => {
+  //when the reducer function is triggered, check for what is the type and act accordingly
+  if (action.type === "ADD") {
+    //push() and unshift() methods alter the existing array
+    //concat returns a new array which is the ideal here
+    //because helps react to track what's happening
+    const updateItems = state.item.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updateItems,
+      totalAmount: updatedTotalAmount,
     };
+  }
+  return defaultCartState;
+};
 
-    return (
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    )
+//the objective of this component is to alter the CartContext and
+//provide its content to the rest of the application
+const CartProvider = (props) => {
+  //the useReducer() hook returns an array with two elements, similar to useState()
+  //but it also expects two arguments. First, the function to be triggered as the reducer
+  //and second, the initial state
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    //when an action is dispatched, it's a convention to give an object as the argument.
+    //the object has a type property with a identifier as a string all CAPS
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
+
+//to work with the context
--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -3,7 +3,21 @@ import React from 'react';
 const CartContext = React.createContext({
     items: [],
     totalAmount: 0,
-    addItem: (item) => {},
+    addItem: (item) => {
+
+        //creating a constant to get the index of the element in the array with the same id from the received item
+        const i = this.items.findIndex(e => e.id === item.id);
+
+        //if the item is already in the array, i is bigger than -1
+        //if the item isn't in the array, i = -1;
+
+        if (i === 0) {
+            this.items.push(item);
+        } else {
+            this.items[i].amount =+ item.amount 
+        }
+        
+    },
     removeItem: (id) => {},
 });
 
